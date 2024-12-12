ecfbdca23ad02a84ac1fb7c82f10db8ac7576153
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-12-13 02:13:26 +0530

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://pumpfunapi.blocktools.ai';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://pumpfunapi.blocktools.ai';
